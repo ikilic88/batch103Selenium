16822794616b710af9058c157b0e63ae89ba7876
ibrahim kılıç <ikilic9188@gmail.com>
2022-12-20 23:54:05 +0300

    18/12/2022

---
+++ src/test/java/day03_Xpath/C02_CssSelector.java
@@ -0,0 +1,24 @@
+package day03_Xpath;
+
+public class C02_CssSelector {
+    public static void main(String[] args) {
+
+
+
+
+
+        //1-C01_TekrarTesti isimli bir class olusturun 2- https://www.amazon.com/ adresine gidin 3- Browseri tam sayfa yapin
+        //4- Sayfayi “refresh” yapin
+        //5Sayfa basliginin “Spend less” ifadesi icerdigini test edin
+        //6Gift Cards sekmesine basin
+        //7Birthday butonuna basin
+        //8 Best Seller bolumunden ilk urunu tiklayin 9- Gift card details’den 25 $’i secin
+        //10-Urun ucretinin 25$ oldugunu test edin
+        //10-Sayfayi kapatin
+
+
+
+
+
+    }
+}
